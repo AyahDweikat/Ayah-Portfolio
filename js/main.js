09c18e24c3b0852289523b7e7b96be8af22b8bf4
AyahDweikat <dweikatayah99@gmail.com>
2023-04-09 05:51:40 +0300

    active Links with scrolling

--- js/main.js
+++ js/main.js
@@ -9,3 +9,25 @@ function myFunction() {
     iconSpan.className = "";
   }
 }
+
+
+// Scrolling
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll("nav ul.nav-list li");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 80) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(`_${current}`)) {
+      li.classList.add("active");
+    }
+  });
+};
